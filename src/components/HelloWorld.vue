fb3f3ffdcae23e271f805f2881d81380d1f87ca1
eggplantiny <eggplantiny@gmail.com>
2020-09-25 11:54:21 +0900

    이게 되네...?

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,151 +1,103 @@
 <template>
-  <v-container>
-    <v-row class="text-center">
-      <v-col cols="12">
-        <v-img
-          :src="require('../assets/logo.svg')"
-          class="my-3"
-          contain
-          height="200"
-        />
-      </v-col>
-
-      <v-col class="mb-4">
-        <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to Vuetify
-        </h1>
-
-        <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br>please join our online
-          <a
-            href="https://community.vuetifyjs.com"
-            target="_blank"
-          >Discord Community</a>
-        </p>
-      </v-col>
-
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-3">
-          What's next?
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(next, i) in whatsNext"
-            :key="i"
-            :href="next.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ next.text }}
-          </a>
+    <v-container fluid>
+        <v-row>
+            <v-col cols="12" class="text-center">
+                <date-range-picker
+                    v-model="dateRange"
+                    :append-to-body="true"
+                    :auto-apply="true"
+                    @update="updateDate"
+                >
+                    <template v-slot:input="picker" style="min-width: 350px;">
+                        {{ picker.startDate | date }} - {{ picker.endDate | date }}
+                    </template>
+                </date-range-picker>
+            </v-col>
+            <v-col cols="12">
+                <grid
+                    ref="grid"
+                    :data="data"
+                    :columns="columns"
+                />
+            </v-col>
         </v-row>
-      </v-col>
-
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-3">
-          Important Links
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(link, i) in importantLinks"
-            :key="i"
-            :href="link.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ link.text }}
-          </a>
-        </v-row>
-      </v-col>
-
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-3">
-          Ecosystem
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(eco, i) in ecosystem"
-            :key="i"
-            :href="eco.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ eco.text }}
-          </a>
-        </v-row>
-      </v-col>
-    </v-row>
-  </v-container>
+    </v-container>
 </template>
 
 <script>
-  export default {
-    name: 'HelloWorld',
+import { Grid } from '@toast-ui/vue-grid'
+import moment from 'moment'
 
-    data: () => ({
-      ecosystem: [
-        {
-          text: 'vuetify-loader',
-          href: 'https://github.com/vuetifyjs/vuetify-loader',
-        },
-        {
-          text: 'github',
-          href: 'https://github.com/vuetifyjs/vuetify',
-        },
-        {
-          text: 'awesome-vuetify',
-          href: 'https://github.com/vuetifyjs/awesome-vuetify',
-        },
-      ],
-      importantLinks: [
-        {
-          text: 'Documentation',
-          href: 'https://vuetifyjs.com',
-        },
-        {
-          text: 'Chat',
-          href: 'https://community.vuetifyjs.com',
-        },
-        {
-          text: 'Made with Vuetify',
-          href: 'https://madewithvuejs.com/vuetify',
-        },
-        {
-          text: 'Twitter',
-          href: 'https://twitter.com/vuetifyjs',
-        },
-        {
-          text: 'Articles',
-          href: 'https://medium.com/vuetify',
-        },
-      ],
-      whatsNext: [
-        {
-          text: 'Explore components',
-          href: 'https://vuetifyjs.com/components/api-explorer',
-        },
-        {
-          text: 'Select a layout',
-          href: 'https://vuetifyjs.com/getting-started/pre-made-layouts',
-        },
-        {
-          text: 'Frequently Asked Questions',
-          href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
-        },
-      ],
-    }),
-  }
+import DateRangePicker from 'vue2-daterange-picker'
+import 'vue2-daterange-picker/dist/vue2-daterange-picker.css'
+
+
+export default {
+    name: 'HelloWorld',
+    components: {
+        Grid,
+        DateRangePicker
+    },
+    data () {
+        return {
+            dateRange: {
+                startDate: moment().startOf('month'),
+                endDate: moment()
+            },
+            columns: [
+                {
+                    name: 'createdAt',
+                    header: '생성일',
+                    filter: {
+                        type: 'date',
+                        operator: 'AND'
+                    }
+                },
+                {
+                    name: 'userName',
+                    header: '사용자명'
+                }
+            ],
+            data: [
+                { createdAt: '2020-09-18', userName: 'eggplantiny1' },
+                { createdAt: '2020-09-11', userName: 'eggplantiny2' },
+                { createdAt: '2020-09-04', userName: 'eggplantiny3' },
+                { createdAt: '2020-09-25', userName: 'eggplantiny4' },
+            ]
+        }
+    },
+    filters: {
+        date (value) {
+            return moment(value).format('YYYY.MM.DD')
+        }
+    },
+    methods: {
+        updateDate ({ startDate, endDate }) {
+            this.$refs.grid.invoke('filter', 'createdAt', [
+                { code: 'afterEq', value: startDate },
+                { code: 'beforeEq', value: endDate }
+            ])
+        }
+    }
+}
 </script>
+
+<style>
+.reportrange-text {
+    border: none;
+    border-radius: 4px;
+    color: #1e1e1e;
+    opacity: 0.8;
+}
+
+.col {
+    -ms-flex-preferred-size: auto !important;
+    flex-basis: auto !important;
+}
+
+.col {
+    -ms-flex-preferred-size: auto !important;
+    flex-basis: auto !important;
+}
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
-import vuetify from './plugins/vuetify';
+import vuetify from './plugins/vuetify'
+import 'tui-grid/dist/tui-grid.css'
+
 
 Vue.config.productionTip = false
 
